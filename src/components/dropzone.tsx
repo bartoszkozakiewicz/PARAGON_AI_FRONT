8fbfc30e53d67cb36f22ad58d6201c7bac81f3c7
bartoszkozakiewicz <bartek@MacBook-Pro-bartoszkozakiewicz.local>
2023-11-23 12:04:11 +0100

    send paragon

--- src/components/dropzone.tsx
+++ src/components/dropzone.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import React, { useCallback } from "react";
+import React, { useCallback,useState } from "react";
 import { useDropzone } from "react-dropzone";
 import Image from "next/image";
 import { axiosInstance } from "@/utils/axiosInstace";
 const path = "http://localhost:5000/api/v1";
 
 export default function MyDropzone() {
+  const [paragon, setParagon] = useState<any>(null);
 
-  const sendParagon = async () =>{
-    await axiosInstance.post(`${path}/paragon/getParagon`, {"msg":"test"})
+  const sendParagon = async (paragon:Buffer) =>{
+    await axiosInstance.post(`${path}/paragon/getParagon`, {"paragonBuffer":paragon})
   }
-  sendParagon()
 
   const onDrop = useCallback((acceptedFiles: any) => {
     acceptedFiles.forEach((file: any) => {
@@ -21,8 +21,15 @@ export default function MyDropzone() {
       reader.onerror = () => console.log("file reading has failed");
       reader.onload = () => {
         // Do whatever you want with the file contents
-        const binaryStr = reader.result;
-        console.log(binaryStr);
+        const paragonArrBuff = reader.result as ArrayBuffer
+        const paragonBuff = Buffer.from(paragonArrBuff);
+
+        console.log(paragonBuff);
+        const rozmiar = paragonBuff.byteLength;
+        console.log('Rozmiar Uint8Array:', rozmiar, 'bajtów');
+        
+        setParagon(paragonBuff)
+        sendParagon(paragonBuff)
       };
       reader.readAsArrayBuffer(file);
     });
